3e02fd7681141c3c6b1583b1f0800a5771502058
PolishchukAlexandr <135067688+PolishchukAlexandr@users.noreply.github.com>
2023-11-26 22:25:44 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import { UserProfile } from 'components/Profile/Profile';
+import user from 'Data/user.json';
+
+import { UserStatistic } from 'components/Statistic/Statistic';
+import data from 'JSON/data.json';
+
+import { Friends } from './Friend-list/Friends';
+import friends from 'JSON/friends.json';
+
+import { TransactionHistoryUser } from 'components/Transaction/Transaction';
+import  transaction from 'JSON/transactions.json';
+
+
 export const App = () => {
   return (
     <div
